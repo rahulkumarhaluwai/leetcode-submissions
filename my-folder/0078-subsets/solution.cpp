0024ad304c7c805eacb90ed55af7a1fd034449c2
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-07-15 07:29:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (99.66%)

---
+++ my-folder/0078-subsets/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void func(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans){
+     if(ind == nums.size()){
+        ans.push_back(ds);
+        return;
+     }
+     ds.push_back(nums[ind]);
+     func(ind + 1, nums, ds, ans);
+     ds.pop_back();
+     func(ind + 1, nums, ds, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        func(0, nums, ds, ans);
+        return ans;
+    }
+};
