7d4f7878276e93895565c7e016a09db7f98d595c
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-07-18 10:17:54 +0000

    [LeetCode Sync] Runtime - 69 ms (62.69%), Memory - 193.7 MB (18.05%)

---
+++ my-folder/0943-sum-of-subarray-minimums/solution.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    vector<int> findNSE(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = !st.empty() ? st.top() : n;
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    vector<int> findPSEE(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = !st.empty() ? st.top() : -1;
+            st.push(i);
+        }
+
+        return ans;
+    }
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int> nse = findNSE(arr);
+        vector<int> psee = findPSEE(arr);
+
+        int n = arr.size();
+        int mod = 1e9 + 7;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+            long long freq = 1LL * left * right;
+            int val = (1LL * freq * arr[i]) % mod;
+            sum = (sum + val) % mod;
+        }
+
+        return sum;
+    }
+};
