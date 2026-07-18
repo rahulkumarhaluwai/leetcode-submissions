ded0e48534e6def4e2dac88c282464ab55f8f3fd
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-07-18 09:48:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.8 MB (48.84%)

---
+++ my-folder/0057-insert-interval/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(intervals[i][1]<newInterval[0])
+            {
+                ans.push_back(intervals[i]);
+            }
+            else if(intervals[i][0]>newInterval[1])
+            {
+                ans.push_back(newInterval);
+                newInterval[0]=intervals[i][0];
+                newInterval[1]=intervals[i][1];
+            }
+            else
+            {
+                 newInterval[0]=min(intervals[i][0],newInterval[0]);
+                newInterval[1]=max(intervals[i][1],newInterval[1]);
+            }
+        }
+        ans.push_back(newInterval);
+        return ans;
+    }
+};
