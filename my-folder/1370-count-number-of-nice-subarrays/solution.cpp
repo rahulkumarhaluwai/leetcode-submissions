1124948fb74747ec5971bb858c133eacdb6e5b0d
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-07-18 09:14:34 +0000

    [LeetCode Sync] Runtime - 4 ms (83.19%), Memory - 71.4 MB (70.10%)

---
+++ my-folder/1370-count-number-of-nice-subarrays/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countAtMost(vector<int>& nums, int k) {
+        int left = 0, res = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] % 2 != 0)
+                k--;
+            while (k < 0) {
+                if (nums[left] % 2 != 0)
+                    k++;
+                left++;
+            }
+            res += (right - left + 1);
+        }
+        return res;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+    }
+};
