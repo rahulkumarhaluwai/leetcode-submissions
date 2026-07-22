061c85b8622751dd50ddb0c89e4bc5fb21a6d6ae
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-07-22 08:54:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.9 MB (39.30%)

---
+++ my-folder/0268-missing-number/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xor1 = 0, xor2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            xor1 ^= i + 1;
+            xor2 ^= nums[i];
+        }
+
+        return xor1 ^ xor2;
+    }
+};
