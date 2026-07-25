491d8be28fc2a0ae1f9d1cd1f1fe1bb4ab59ecae
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-07-25 08:32:51 +0000

    [LeetCode Sync] Runtime - 1 ms (97.23%), Memory - 44.8 MB (78.99%)

--- my-folder/0455-assign-cookies/solution.cpp
+++ my-folder/0455-assign-cookies/solution.cpp
@@ -3,14 +3,14 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        int gIndex = 0; 
-        int sIndex = 0;  
-        while (gIndex < g.size() && sIndex < s.size()) {
-            if (s[sIndex] >= g[gIndex]) {
-                gIndex++; 
+        int l = 0;
+        int r = 0;
+        while (l < g.size() && r < s.size()) {
+            if (g[l] <= s[r]) {
+                l++;
             }
-            sIndex++; 
+            r++;
         }
-        return gIndex;
+        return l;
     }
 };
