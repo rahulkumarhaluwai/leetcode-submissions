a9bd18a7a0491df0ee7aa9f07e3d65a842685a62
Rahul Kumar Haluwai <rahulkumarhaluwai@gmail.com>
2026-08-11 04:30:44 +0000

    [LeetCode Sync] Runtime - 7 ms (45.40%), Memory - 19.1 MB (90.63%)

--- my-folder/0207-course-schedule/solution.cpp
+++ my-folder/0207-course-schedule/solution.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& visited,
-             vector<int>& pathVisited) {
-        visited[node] = 1;
-        pathVisited[node] = 1;
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, adj, visited, pathVisited))
-                    return true;
-            }
-            else if (pathVisited[neighbor]) {
-                return true;
-            }
-        }
-        pathVisited[node] = 0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for (auto &p : prerequisites) {
-            adj[p[1]].push_back(p[0]);
+        vector<int> inDegree(numCourses, 0);
+        for (auto& pre : prerequisites) {
+            int a = pre[0], b = pre[1];
+            adj[b].push_back(a);
+            inDegree[a]++;
         }
-        vector<int> visited(numCourses, 0);
-        vector<int> pathVisited(numCourses, 0);
-
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (dfs(i, adj, visited, pathVisited))
-                    return false;
+            if (inDegree[i] == 0) {
+                q.push(i);
             }
         }
-        return true;
+        int count = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            count++;
+            for (int nei : adj[node]) {
+                inDegree[nei]--;
+                if (inDegree[nei] == 0) {
+                    q.push(nei);
+                }
+            }
+        }
+        return count == numCourses;
     }
 };
